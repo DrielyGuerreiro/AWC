61d85e925903cee930d53d0a94d43e561ecde789
Driely Guerreiro <drielycristine@rocketmail.com>
2021-02-26 22:41:48 -0300

    aula-26-02-2021

---
+++ app/Models/Funcionario.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Funcionario extends Model
+{
+    use HasFactory;
+
+    protected $filable = [ 'id',
+                            'nome',
+                            'endereço',
+                            'email',
+                            'telefone'];
+    protected $table = 'Funcionario';                        
+}
