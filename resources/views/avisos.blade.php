9e4a485bf5215407ac2cddd6e38abfd850561da5
DrielyGuerreiro <68483756+DrielyGuerreiro@users.noreply.github.com>
2021-03-12 22:38:47 -0300

    introducao a view

---
+++ resources/views/avisos.blade.php
@@ -0,0 +1,9 @@
+@extends ('layouts.externo')
+@section ('title','Quadro de Novidades')
+@section ('sidebar')
+    @parent
+    <p>^ ^ Barra superior adicionada do layout pai/m&atilde;e ^ ^ </p>
+@endsection
+@section('content')
+    <p>Quadro de Novidades da Empresa</p>
+@endsection
